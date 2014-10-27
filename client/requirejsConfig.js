940d696c8dcb4c5ac57910e796c0cfea56a880ff
m-j <jamiolkowski.mateusz@gmail.com>
2014-10-27 18:41:25 +0100

    Adding jquery

--- client/requirejsConfig.js
+++ client/requirejsConfig.js
@@ -6,13 +6,15 @@ aes.requirejsConfig = function () {
             "restangular": "libs/restangular/dist/restangular",
             "ui-router": "libs/ui-router/release/angular-ui-router",
             "lodash": "libs/lodash/dist/lodash",
-            "angular-mocks" : "libs/angular-mocks/angular-mocks"
+            "angular-mocks" : "libs/angular-mocks/angular-mocks",
+            "jquery" : "libs/jquery/dist/jquery"
         },
         shim: {
-            angular: { exports: 'angular', deps: ['lodash']},
+            angular: { exports: 'angular', deps: ['lodash', 'jquery']},
             restangular: ['angular'],
             "ui-router": ['angular'],
-            "angular-mocks" : { exports : 'angular.mock', deps : ['angular']}
+            "angular-mocks" : { exports : 'angular.mock', deps : ['angular']},
+            "jquery" : {exports : 'jQuery'}
         }
     }
 }
